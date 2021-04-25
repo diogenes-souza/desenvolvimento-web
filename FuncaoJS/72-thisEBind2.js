34f36ceef9dc584e1d7c542570e928bd35cb0987
Diógenes Souza <diogenes.edusouza@gmail.com>
2021-04-25 19:45:58 -0300

    _20210425
    Atualização do curso de desenvolvimento web

---
+++ FuncaoJS/72-thisEBind2.js
@@ -0,0 +1,14 @@
+// this e bind - Ex. 2
+
+  
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
+}
+
+new Pessoa
---
+++ FuncaoJS/74-arrowFunction2.js
@@ -0,0 +1,14 @@
+/**
+ * Arrow Function - Ex. 2
+ */
+
+ function Pessoa() {
+    this.idade = 0
+
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Pessoa
